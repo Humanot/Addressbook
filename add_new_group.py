be30a50f7b07083ef24fda19d327a37632d4658c
George Mironov <yegor.mironovwill@gmail.com>
2019-09-24 12:00:39 +0300

    parametrized test methods and added one more test "create_emty_group"

--- add_new_group.py
+++ add_new_group.py
@@ -9,11 +9,19 @@ class add_new_group(unittest.TestCase):
     
     def test_add_new_group(self):
         driver = self.driver
-
         self.open_home_page(driver)
-        self.login(driver)
+        self.login(driver, username="admin", password="secret")
         self.open_groups_page(driver)
-        self.create_group(driver)
+        self.create_group(driver, name="First", header="somt", footer="ghy")
+        self.return_to_groups_page(driver)
+        self.logout(driver)
+
+    def test_add_empty_group(self):
+        driver = self.driver
+        self.open_home_page(driver)
+        self.login(driver, username="admin", password="secret")
+        self.open_groups_page(driver)
+        self.create_group(driver, name="", header="", footer="")
         self.return_to_groups_page(driver)
         self.logout(driver)
 
@@ -23,19 +31,19 @@ class add_new_group(unittest.TestCase):
     def return_to_groups_page(self, driver):
         driver.find_element_by_link_text("group page").click()
 
-    def create_group(self, driver):
+    def create_group(self, driver, name, header, footer):
         driver.find_element_by_name("new").click()
 
         # Fill group form
         driver.find_element_by_name("group_name").click()
         driver.find_element_by_name("group_name").clear()
-        driver.find_element_by_name("group_name").send_keys("First")
+        driver.find_element_by_name("group_name").send_keys(name)
         driver.find_element_by_name("group_header").click()
         driver.find_element_by_name("group_header").clear()
-        driver.find_element_by_name("group_header").send_keys("somt")
+        driver.find_element_by_name("group_header").send_keys(header)
         driver.find_element_by_name("group_footer").click()
         driver.find_element_by_name("group_footer").clear()
-        driver.find_element_by_name("group_footer").send_keys("ghy")
+        driver.find_element_by_name("group_footer").send_keys(footer)
 
         # submit group creation
         driver.find_element_by_name("submit").click()
@@ -44,12 +52,12 @@ class add_new_group(unittest.TestCase):
         # open groups page
         driver.find_element_by_link_text("groups").click()
 
-    def login(self, driver):
+    def login(self, driver, username, password):
         driver.find_element_by_name("user").clear()
-        driver.find_element_by_name("user").send_keys("admin")
+        driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").click()
         driver.find_element_by_name("pass").clear()
-        driver.find_element_by_name("pass").send_keys("secret")
+        driver.find_element_by_name("pass").send_keys(password)
         driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
 
