82ea421f98da3bac46a29ebb6541ee49a5be6641
George Mironov <yegor.mironovwill@gmail.com>
2019-09-26 10:20:15 +0300

    Added extracting driver in test methods

--- add_new_group.py
+++ add_new_group.py
@@ -10,26 +10,28 @@ class add_new_group(unittest.TestCase):
     
     def test_add_new_group(self):
         driver = self.driver
-        self.login(driver, username="admin", password="secret")
-        self.create_group(driver, Group(name="First", header="somt", footer="ghy"))
-        self.logout(driver)
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="First", header="somt", footer="ghy"))
+        self.logout()
 
     def test_add_empty_group(self):
         driver = self.driver
-        self.login(driver, username="admin", password="secret")
-        self.create_group(driver, Group(name="", header="", footer=""))
-        self.return_to_groups_page(driver)
-        self.logout(driver)
+        self.login(username="admin", password="secret")
+        self.create_group(Group(name="", header="", footer=""))
+        self.return_to_groups_page()
+        self.logout()
 
-    def logout(self, driver):
+    def logout(self):
+        driver = self.driver
         driver.find_element_by_link_text("Logout").click()
 
-    def return_to_groups_page(self, driver):
+    def return_to_groups_page(self):
+        driver = self.driver
         driver.find_element_by_link_text("group page").click()
 
-    def create_group(self, driver, group):
-
-        self.open_groups_page(driver)
+    def create_group(self, group):
+        driver = self.driver
+        self.open_groups_page()
         driver.find_element_by_name("new").click()
         # Fill group form
         driver.find_element_by_name("group_name").click()
@@ -44,14 +46,16 @@ class add_new_group(unittest.TestCase):
 
         # submit group creation
         driver.find_element_by_name("submit").click()
-        self.return_to_groups_page(driver)
+        self.return_to_groups_page()
 
-    def open_groups_page(self, driver):
+    def open_groups_page(self):
         # open groups page
+        driver = self.driver
         driver.find_element_by_link_text("groups").click()
 
-    def login(self, driver, username, password):
-        self.open_home_page(driver)
+    def login(self, username, password):
+        driver = self.driver
+        self.open_home_page()
         driver.find_element_by_name("user").clear()
         driver.find_element_by_name("user").send_keys(username)
         driver.find_element_by_name("pass").click()
@@ -60,7 +64,8 @@ class add_new_group(unittest.TestCase):
         driver.find_element_by_xpath(
             "(.//*[normalize-space(text()) and normalize-space(.)='Password:'])[1]/following::input[2]").click()
 
-    def open_home_page(self, driver):
+    def open_home_page(self):
+        driver = self.driver
         driver.get("http://localhost/addressbook/")
 
     def tearDown(self):
