192cf3d8468b67c08e386c89f7f6ae5710edfad7
George Mironov <yegor.mironovwill@gmail.com>
2020-01-21 02:22:21 +0300

    Add preconditions for creating a group before deleting/modifying and a method for counting items in a list group

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,4 +1,10 @@
 from model.group import Group
 
 def test_edit_group_name(app):
+    validate_group_exists(app)
     app.group.edit_first(Group(name="Fucking brandnew name"))
+
+
+def validate_group_exists(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test"))
