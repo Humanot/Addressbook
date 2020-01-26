497b3f22551fe9d1ab582073b852f81032272b16
George Mironov <yegor.mironovwill@gmail.com>
2020-01-26 22:58:08 +0300

    Optimize eq method to ignore None id and add comparison old and new group list in edit/add groups tests with their sorting

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -3,9 +3,14 @@ from model.group import Group
 def test_edit_group_name(app):
     validate_group_exists(app)
     old_groups = app.group.get_group_list()
-    app.group.edit_first(Group(name="Fucking brandnew name"))
+    group = Group(name="brandnew name")
+    group.id = old_groups[0].id
+    app.group.edit_first(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) == len(new_groups)
+    old_groups[0] = group
+    print(old_groups[0])
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def validate_group_exists(app):
